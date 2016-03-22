941ff4fb7dd947029a611a67b2851289f0785baf
mvolta <marcelovolta@gmail.com>
2016-03-22 08:32:32 -0300

    First version: simple addition

--- TestParaGitHub/Program.cs
+++ TestParaGitHub/Program.cs
@@ -10,6 +10,11 @@ namespace TestParaGitHub
     {
         static void Main(string[] args)
         {
+            int x = 1;
+            int y = 2;
+            int z = x + y;
+            Console.WriteLine(z);
+            Console.ReadLine();
         }
     }
 }
